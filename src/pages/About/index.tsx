19ef2dea0a4a962b869dc96ac19ebe812e4ff93a
HeySankalp <sankalpsachan1@gmail.com>
2023-04-14 15:24:28 +0530

    pwa implementation

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -9,22 +9,22 @@ const About = () => {
 
     return (
         <div>
-            <div className='w-full h-[300px]'>
+            {/* <div className='w-full h-[300px]'>
                 <ImageCarousal
                     navButtons={true}
                     imagesArray={images}
                     showPagination={true}
                     objectFit={"contain"}
                 />
-            </div>
-            <div className='p-10'>
+            </div> */}
+            {/* <div className='p-10'>
                 <h1 className='text-lg font-semibold'>About US</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, voluptatibus nisi tempore ipsum unde quos vitae vero magnam soluta corrupti recusandae ducimus officia fuga dicta qui architecto eveniet ut, veritatis nostrum ea earum nihil perspiciatis pariatur quae? Tenetur nemo doloremque cum, quia explicabo veniam reiciendis excepturi enim perspiciatis magnam blanditiis et dignissimos deserunt similique non quasi! Iste quo fuga saepe eaque illo necessitatibus inventore voluptatem iure deserunt vel, porro ab adipisci ipsa excepturi amet voluptatum! Placeat consequatur veniam, laudantium maxime at illo magni nulla vel aliquam porro eaque cum cupiditate doloremque, molestias natus provident minima repudiandae id aspernatur nobis eum!</p>
             </div>
             <div className='p-10'>
                 <h1 className='text-lg font-semibold'>Our Policy</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, voluptatibus nisi tempore ipsum unde quos vitae vero magnam soluta corrupti recusandae ducimus officia fuga dicta qui architecto eveniet ut, veritatis nostrum ea earum nihil perspiciatis pariatur quae? Tenetur nemo doloremque cum, quia explicabo veniam reiciendis excepturi enim perspiciatis magnam blanditiis et dignissimos deserunt similique non quasi! Iste quo fuga saepe eaque illo necessitatibus inventore voluptatem iure deserunt vel, porro ab adipisci ipsa excepturi amet voluptatum! Placeat consequatur veniam, laudantium maxime at illo magni nulla vel aliquam porro eaque cum cupiditate doloremque, molestias natus provident minima repudiandae id aspernatur nobis eum! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia autem minus sit nemo beatae perferendis nihil sapiente! Asperiores, consequatur deleniti! Natus officia consectetur distinctio eius! Fugit, reprehenderit numquam tempora obcaecati nesciunt recusandae quae eaque amet impedit, magni assumenda eveniet deleniti!</p>
-            </div>
+            </div> */}
         </div>
     );
 }
