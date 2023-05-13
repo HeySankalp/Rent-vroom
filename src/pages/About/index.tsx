301763b92560b1e7153e5a8b38b0c79cf5237dd1
HeySankalp <sankalpsachan1@gmail.com>
2023-05-13 16:49:19 +0530

    qr testing

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,11 +1,27 @@
 import React, { useState } from 'react'
 import { images } from '../../Components/Content/db';
 import ImageCarousal from '../../Components/ImageCarousel/ImageCarousel';
-
+import Qrscanner from 'react-qr-scanner';
 
 const About = () => {
+    const [setFace, face] = useState<any>("front");
+    const handleError = (error: any) => {
+        console.log(error);
+    };
 
+    const previewStyle = {
+        height: 520,
+        width: 640,
+        objectFit: "cover",
+    };
 
+    const handleScan = (data: { text: string; }) => {
+        if (data?.text) {
+            let cred = data?.text?.split("/");
+            let screeningId = cred[cred.length - 1];
+            let eventId = cred[cred.length - 2];
+        }
+    };
 
     return (
         <div>
@@ -17,6 +33,16 @@ const About = () => {
                     objectFit={"contain"}
                 />
             </div>
+            <Qrscanner
+                delay={1000}
+                style={previewStyle}
+                onError={handleError}
+                onScan={handleScan}
+                facingMode={face}
+            />
+
+            <button onClick={()=>{setFace("front")}}>front</button>
+            <button onClick={()=>{setFace("rear")}}></button>
             <div className='p-10'>
                 <h1 className='text-lg font-semibold'>About US</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, voluptatibus nisi tempore ipsum unde quos vitae vero magnam soluta corrupti recusandae ducimus officia fuga dicta qui architecto eveniet ut, veritatis nostrum ea earum nihil perspiciatis pariatur quae? Tenetur nemo doloremque cum, quia explicabo veniam reiciendis excepturi enim perspiciatis magnam blanditiis et dignissimos deserunt similique non quasi! Iste quo fuga saepe eaque illo necessitatibus inventore voluptatem iure deserunt vel, porro ab adipisci ipsa excepturi amet voluptatum! Placeat consequatur veniam, laudantium maxime at illo magni nulla vel aliquam porro eaque cum cupiditate doloremque, molestias natus provident minima repudiandae id aspernatur nobis eum!</p>
