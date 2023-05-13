12d1ff0ce7a725e1467fb5c1ef8003db9e4f54dc
HeySankalp <sankalpsachan1@gmail.com>
2023-05-13 17:03:42 +0530

    qr testing+2

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -42,7 +42,7 @@ const About = () => {
             />
 
             <button onClick={()=>{setFace("front")}}>front</button>
-            <button onClick={()=>{setFace("rear")}}></button>
+            <button onClick={()=>{setFace("rear")}}>back</button>
             <div className='p-10'>
                 <h1 className='text-lg font-semibold'>About US</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, voluptatibus nisi tempore ipsum unde quos vitae vero magnam soluta corrupti recusandae ducimus officia fuga dicta qui architecto eveniet ut, veritatis nostrum ea earum nihil perspiciatis pariatur quae? Tenetur nemo doloremque cum, quia explicabo veniam reiciendis excepturi enim perspiciatis magnam blanditiis et dignissimos deserunt similique non quasi! Iste quo fuga saepe eaque illo necessitatibus inventore voluptatem iure deserunt vel, porro ab adipisci ipsa excepturi amet voluptatum! Placeat consequatur veniam, laudantium maxime at illo magni nulla vel aliquam porro eaque cum cupiditate doloremque, molestias natus provident minima repudiandae id aspernatur nobis eum!</p>
